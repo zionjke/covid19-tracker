f70aad2faa50a3b068669ed1eda11e012d21a5d1
zionjke <vincere802@gmail.com>
2020-09-05 15:44:44 +0300

    Updates

---
+++ precache-manifest.4f62d9a7e1afdc1d53d7c1a3b4fae940.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ec8d9f103e9f7fbbacb583d06fcb767",
+    "url": "/covid19-tracker/index.html"
+  },
+  {
+    "revision": "8fd7babc3689c503178d",
+    "url": "/covid19-tracker/static/css/2.bcddf60d.chunk.css"
+  },
+  {
+    "revision": "bfcdda64be178975ef59",
+    "url": "/covid19-tracker/static/css/main.12a70e71.chunk.css"
+  },
+  {
+    "revision": "8fd7babc3689c503178d",
+    "url": "/covid19-tracker/static/js/2.54a9cd7c.chunk.js"
+  },
+  {
+    "revision": "ebc70c097554e9e2ef968c42fad2beba",
+    "url": "/covid19-tracker/static/js/2.54a9cd7c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bfcdda64be178975ef59",
+    "url": "/covid19-tracker/static/js/main.68dd1b1d.chunk.js"
+  },
+  {
+    "revision": "6b444aef87d753a0bf04",
+    "url": "/covid19-tracker/static/js/runtime-main.815270a9.js"
+  }
+]);
